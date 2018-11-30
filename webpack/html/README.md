2bcbf7aa05f9ced5cc4333a3e1929dc5ded6ba54
cloped <educarvalholopes98@gmail.com>
2018-11-30 08:24:37 -0400

    add html anotations webpack

---
+++ webpack/html/README.md
@@ -0,0 +1,6 @@
+# Some anotations of html loader config webpack
+
+- `html-loader`: used to transform html in text
+- `html-webpack-plugin`: used to create the files html in the dist
+- `file-loader`: used to put images in dist
+
---
+++ webpack/html/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); // generate the file html
+
+module.exports = {
+  entry : ['@babel/polyfill', './app-src/app.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]', // to keep the name of the image
+              outputPath: 'img/', // create a folder inside dist
+              publicPath: 'img/' // to access from html (not sure if necessary)
+            }
+          }
+        ]
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'layout/index.html'
+    }),
+  ]
+}
+
